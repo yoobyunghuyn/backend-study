3129bb921158c836eb8fc6977643145c34601650
ybh <bhyu@bricksoft.co.kr>
2023-06-23 17:37:06 +0900

    .

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -48,7 +48,7 @@ class MemberServiceIntegrationTest {
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다. --2");
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
 
         
         // then
